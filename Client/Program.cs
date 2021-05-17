c44b80a469915306e6d82fcea3d2077dd1b63258
dodgycode <owenpyrah@hotmail.com>
2021-05-17 14:59:34 +0100

    Some auth infrastructure.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,9 @@
+using Client.Infrastructure;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Client
@@ -18,6 +16,9 @@ namespace Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, TestAuthStateProvider>();
 
             await builder.Build().RunAsync();
         }
