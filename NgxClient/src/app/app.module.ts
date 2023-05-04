d5a4cc32ed3f3ac56d40f50d2626ed8f4a9a3733
owen <owen.pyrah@livehire.com>
2023-05-04 16:36:10 +0800

    Added angular/material.

--- NgxClient/src/app/app.module.ts
+++ NgxClient/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SeasonComponent } from './season/season.component';
 import { SeriesComponent } from './series/series.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { SeriesComponent } from './series/series.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
